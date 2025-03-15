80fbc5f88586acbd72fc4125e834baaf3ab727dd
Saniya-k-s <saniminnu04@gmail.com>
2025-03-15 08:06:51 +0000

    mysql connectivity

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -84,10 +84,21 @@ WSGI_APPLICATION = 'crm.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'crm_db',
+        'USER': 'crmadmin',
+        'PASSWORD': 'crmadmin@2024',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
